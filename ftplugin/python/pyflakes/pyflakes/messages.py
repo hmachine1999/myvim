4ebfe0f4a9e2978a4ca80203eeab96821cc83264
ryancosta <hansonbbmak@hotmail.com>
2016-09-13 21:42:33 +0800

    .vimrc

--- ftplugin/python/pyflakes/pyflakes/messages.py
+++ ftplugin/python/pyflakes/pyflakes/messages.py
@@ -1,26 +1,41 @@
-# (c) 2005 Divmod, Inc.  See LICENSE file for details
+"""
+Provide the class Message and its subclasses.
+"""
+
 
 class Message(object):
     message = ''
     message_args = ()
-    def __init__(self, filename, loc, use_column=True):
+
+    def __init__(self, filename, loc):
         self.filename = filename
         self.lineno = loc.lineno
-        self.col = getattr(loc, 'col_offset', None) if use_column else None
+        self.col = getattr(loc, 'col_offset', 0)
 
     def __str__(self):
-        return '%s:%s: %s' % (self.filename, self.lineno, self.message % self.message_args)
+        return '%s:%s: %s' % (self.filename, self.lineno,
+                              self.message % self.message_args)
 
 
 class UnusedImport(Message):
     message = '%r imported but unused'
+
     def __init__(self, filename, loc, name):
-        Message.__init__(self, filename, loc, use_column=False)
+        Message.__init__(self, filename, loc)
         self.message_args = (name,)
 
 
 class RedefinedWhileUnused(Message):
     message = 'redefinition of unused %r from line %r'
+
+    def __init__(self, filename, loc, name, orig_loc):
+        Message.__init__(self, filename, loc)
+        self.message_args = (name, orig_loc.lineno)
+
+
+class RedefinedInListComp(Message):
+    message = 'list comprehension redefines %r from line %r'
+
     def __init__(self, filename, loc, name, orig_loc):
         Message.__init__(self, filename, loc)
         self.message_args = (name, orig_loc.lineno)
@@ -28,6 +43,7 @@ class RedefinedWhileUnused(Message):
 
 class ImportShadowedByLoopVar(Message):
     message = 'import %r from line %r shadowed by loop variable'
+
     def __init__(self, filename, loc, name, orig_loc):
         Message.__init__(self, filename, loc)
         self.message_args = (name, orig_loc.lineno)
@@ -35,6 +51,7 @@ class ImportShadowedByLoopVar(Message):
 
 class ImportStarUsed(Message):
     message = "'from %s import *' used; unable to detect undefined names"
+
     def __init__(self, filename, loc, modname):
         Message.__init__(self, filename, loc)
         self.message_args = (modname,)
@@ -42,22 +59,34 @@ class ImportStarUsed(Message):
 
 class UndefinedName(Message):
     message = 'undefined name %r'
+
     def __init__(self, filename, loc, name):
         Message.__init__(self, filename, loc)
         self.message_args = (name,)
 
+
+class DoctestSyntaxError(Message):
+    message = 'syntax error in doctest'
+
+    def __init__(self, filename, loc, position=None):
+        Message.__init__(self, filename, loc)
+        if position:
+            (self.lineno, self.col) = position
+        self.message_args = ()
 
 
 class UndefinedExport(Message):
     message = 'undefined name %r in __all__'
+
     def __init__(self, filename, loc, name):
         Message.__init__(self, filename, loc)
         self.message_args = (name,)
 
 
-
 class UndefinedLocal(Message):
-    message = "local variable %r (defined in enclosing scope on line %r) referenced before assignment"
+    message = ('local variable %r (defined in enclosing scope on line %r) '
+               'referenced before assignment')
+
     def __init__(self, filename, loc, name, orig_loc):
         Message.__init__(self, filename, loc)
         self.message_args = (name, orig_loc.lineno)
@@ -65,13 +94,15 @@ class UndefinedLocal(Message):
 
 class DuplicateArgument(Message):
     message = 'duplicate argument %r in function definition'
+
     def __init__(self, filename, loc, name):
         Message.__init__(self, filename, loc)
         self.message_args = (name,)
 
 
-class RedefinedFunction(Message):
-    message = 'redefinition of function %r from line %r'
+class Redefined(Message):
+    message = 'redefinition of %r from line %r'
+
     def __init__(self, filename, loc, name, orig_loc):
         Message.__init__(self, filename, loc)
         self.message_args = (name, orig_loc.lineno)
@@ -79,6 +110,7 @@ class RedefinedFunction(Message):
 
 class LateFutureImport(Message):
     message = 'future import(s) %r after other statements'
+
     def __init__(self, filename, loc, names):
         Message.__init__(self, filename, loc)
         self.message_args = (names,)
@@ -89,8 +121,8 @@ class UnusedVariable(Message):
     Indicates that a variable has been explicity assigned to but not actually
     used.
     """
-
     message = 'local variable %r is assigned to but never used'
+
     def __init__(self, filename, loc, names):
         Message.__init__(self, filename, loc)
         self.message_args = (names,)
